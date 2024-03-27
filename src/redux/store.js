d58a4a41565d0bfcacae7438b48bad9034a35a94
AnnaRychkova1 <aanytkaa@gmail.com>
2024-03-27 15:51:48 +0000

    sixth module

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,27 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { rootReducer } from './reducer';
+// import { createStore } from 'redux';
+// import { devToolsEnhancer } from '@redux-devtools/extension';
+// import { rootReducer } from './reducer';
 
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import { tasksReducer, filtersReducer } from './reducers';
+
+// export const store = configureStore({
+//   reducer: {
+//     tasks: tasksReducer,
+//     filters: filtersReducer,
+//   },
+// });
+
+import { tasksReducer } from './tasksSlice';
+import { filtersReducer } from './filtersSlice';
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
+});
